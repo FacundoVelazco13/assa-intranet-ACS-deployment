afbdcd03aa1856c5060f24761d1f808882cf7a77
FacundoVelazco <facundovelazco133@gmail.com>
2026-02-02 10:02:18 -0300

    actualizando volumenes 4

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 set -e
 
 #FILE="assa-compose.yaml"
-#VOLUMENES=(alfresco-data postgres-data solr-data)
+#VOLUMENES=(alfresco-data postgres-data solr-data solr-home solr-keystores activemq-conf activemq-data activemq-log)
 FILE="dev-compose.yaml"
 VOLUMENES=(alf-dev-data postgres-dev-data solr-dev-data solr-dev-home solr-dev-keystores activemq-dev-conf activemq-dev-data activemq-dev-log openldap-data openldap-config phpldapadmin)
 case "$1" in
